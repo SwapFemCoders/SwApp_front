3edf5fd7ab9fc9f2ec4c7433d3397cfb0075aa9e
ManonChab <godfroymanon@gmail.com>
2026-02-28 20:56:35 +0100

    SWAP-42 Logo-DONE

--- src/components/templates/PageTemplate/PageTemplate.jsx
+++ src/components/templates/PageTemplate/PageTemplate.jsx
@@ -1,9 +1,13 @@
+import Logo from "../../atoms/Logo/Logo";
 import Navbar from "../../molecules/Navbar/Navbar";
 import style from "./PageTemplate.module.css";
 
 const Page = () => (
     <>
         <div className={style.body}>
+            <div className={style.logo}>
+                <Logo/>
+            </div>
             <div className={style.navbar}>
                 <Navbar/>
             </div>
