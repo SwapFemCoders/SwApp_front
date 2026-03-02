3d83111f3c10da8cbfbcb5f2d5613586fcff78ae
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-02 14:30:53 +0100

    feat-SWAP-51 plus button-create

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PlusButton from '../components/atoms/plusButton/PlusButton'
+import ProductCard from '../components/molecules/productCard/ProductCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +29,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more (esto es una prueba para hacer la rama dev)
+      <PlusButton />
+      <ProductCard />
       </p>
     </>
   )
