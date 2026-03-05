a6743804661034bc0fa4ce7acda8672781c038ae
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-05 08:31:01 +0100

    fix use auth and modal(not finished)

--- src/components/atoms/favoriteButton/FavoriteButton.jsx
+++ src/components/atoms/favoriteButton/FavoriteButton.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
+import { useAuth } from '../../../context/AuthContext';
+import { useModal } from '../../../context/ModalContext';
 import redHeart from '../../../assets/images/red-heart.png';
 import whiteHeart from '../../../assets/images/white-heart.jpg';
 import styles from './favorite-button.module.css';
 
-const FavoriteButton = ({ articleId, isLogged, onShowLogin }) => {
+const FavoriteButton = ({ articleId }) => {
     const [isFavorite, setIsFavorite] = useState(false);
+
+    const { isLogged } = useAuth(); 
+    const { openLoginModal } = useModal();
 
     const handleFavClick = (e) => {
         e.stopPropagation(); 
 
         if (!isLogged) {
-            return onShowLogin();
+            return openLoginModal();
         }
         try{
             setIsFavorite(!isFavorite);
+            //llamada a la API para guardar favorito
             console.log("Saving...");
         }catch (error){
             console.error("error in saved", error);
