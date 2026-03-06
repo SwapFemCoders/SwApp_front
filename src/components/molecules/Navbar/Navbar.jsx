2d77016b70605b55b4361d11e2d726f5b971dbb7
ManonChab <godfroymanon@gmail.com>
2026-03-06 09:09:08 +0100

    feat : Created other pages structure but empty

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.css";
 const Navbar = () => (
     <>
         <div className={styles.bar} alt="swapp logo">         
-            <NavbarButton name="SIGN IN" url="/SignIn" />
+            <NavbarButton name="SIGN UP" url="/SignUp" />
             <NavbarButton name="LOG IN" url="/LogIn"/>
             <NavbarButton name="SHOP" url="/Shop"/>
             <NavbarButton name="PROFILE" url="/Profile"/>
