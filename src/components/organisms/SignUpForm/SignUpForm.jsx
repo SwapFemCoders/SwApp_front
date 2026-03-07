9dc9ba783073c10a9e1b2ee787a86ae40d390392
ManonChab <godfroymanon@gmail.com>
2026-03-07 13:55:59 +0100

    feat : SWAP-92 SignUp Form- DONE without validation

--- src/components/organisms/SignUpForm/SignUpForm.jsx
+++ src/components/organisms/SignUpForm/SignUpForm.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react'
 import TitleBox from "../../atoms/titleBox/TitleBox"
 import style from "./SignUpForm.module.css"
-import { Form } from 'react-router'
-import setUsers, { Users } from '../Users/Users'
+import UserPath from '../../../services/UserPath';
 
 export const SignUpForm = () => {
 
-    const [form, setForm] = useState({name:"",
-                                    LastName:"", 
-                                    Username:"", 
-                                    Email:"", 
-                                    Location:"", 
-                                    picture:null});
+    const [form, setForm] = useState({
+        name: "",
+        lastName: "",
+        username: "",
+        email: "",
+        location: "",
+        picture: null
+    });
 
     const handleChange = (event) => {
         setForm({
             ...form,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.files 
+            ? event.target.files[0] 
+            : event.target.value
         })
     }
 
@@ -32,66 +35,88 @@ export const SignUpForm = () => {
         if (form.picture) {
         data.append("picture", form.picture);
         }
-        const response = await UserService.createUser(data);
-
+        const response = await UserPath.createUser(data);
         console.log("User created:", response);
-
         alert("User successfully created!");
-
         handleCancel();
+
     } catch (error) {
         console.error("Signup failed:", error);
         alert("Error creating user");
     }
     };
 
-    };
-
     const handleCancel = () => {
-    setFormData({
+    setForm({
         name: "",
-        lastname: "",
+        lastName: "",
         username: "",
         email: "",
         location: "",
-        picture: null,
+        picture: null
     });
+    };
+
+    const [fileName, setfileName] = useState(" ");
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+
+        if (file) {
+            setfileName(file.name);
+        }
     };
 
     return (
     <>
-        <h1 className={style.title}>
-            <TitleBox text="SIGN UP"/>
-        </h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" name='Name' id="Name" onChange={handleChange} required/>
+        <section className={style.content}>
+            <div className={style.title}>
+                <TitleBox text="SIGN UP"/>
             </div>
-            <div>
-                <label htmlFor="name">Lastname</label>
-                <input type="text" name='Lastname' id="Lastname"  onChange={handleChange} required/>
-            </div>
-            <div>
-                {/* VALIDATION OF THE USERNAME - NEED TO BE UNIQUE - CHECK THE API */}
-                <label htmlFor="name">UserName</label>
-                <input type="text" name='UserName' id="UserName"  onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="name">Email</label>
-                <input type="text" name='Email' id="Email"  onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="name">Location</label>
-                <input type="text" name='Location' id="Location"  onChange={handleChange} required/>
-            </div>
-            <div>
-                <label htmlFor="name">Profile Picture</label>
-                <input type="text" name='picture' id="picture"  onChange={handleChange} required/>
-            </div>
-            <button type="submit">SIGN UP</button>
-        </form>
+            <form className={style.form} onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name" className={style.label}>Name</label>
+                    <input className={style.input} type="text" name='name' id="name" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    <label htmlFor="lastname" className={style.label}>Lastname</label>
+                    <input className={style.input} type="text" name='lastname' id="lastname" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    {/* VALIDATION OF THE USERNAME - NEED TO BE UNIQUE - CHECK THE API */}
+                    <label htmlFor="userName" className={style.label}>UserName</label>
+                    <input className={style.input} type="text" name='userName' id="userName" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    <label htmlFor="email" className={style.label}>Email</label>
+                    <input className={style.input} type="text" name='email' id="email" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    <label htmlFor="password" className={style.label}>Password</label>
+                    <input className={style.input} type="password" name='password' id="password" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    <label htmlFor="name" className={style.label}>Location</label>
+                    <input className={style.input} type="text" name='location' id="location" autoComplete='off' onChange={handleChange} required/>
+                </div>
+                <div>
+                    <label htmlFor="picture" className={style.label}>Picture</label>
+                    <input type="file" name='picture' id="picture" hidden onChange={handleFileChange} autoComplete='off'/>
+                    <label htmlFor="picture" className={style.pictureButton}>UPLOAD</label>
+                    <span className={style.file}>{fileName}</span>
+                </div>
+
+                <section className={style.buttonSection}>
+                    <button  className={style.button} type="submit">SIGN UP</button>
+                    <button  className={style.button} >CANCEL</button>
+                </section>
+            </form>
+        </section>
     </>
     
     )
+
+    console.log(form);
+    };
+
 
