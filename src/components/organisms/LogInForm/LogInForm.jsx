15c6f00296a7840232178c02533549b863115abd
ManonChab <godfroymanon@gmail.com>
2026-03-08 14:03:02 +0100

    feat :SWAP-59 Log-in Page- LogIn logic implemented

--- src/components/organisms/LogInForm/LogInForm.jsx
+++ src/components/organisms/LogInForm/LogInForm.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./LogInForm.module.css"
 import TitleBox from '../../atoms/titleBox/TitleBox';
 
 export const LogInForm = () => {
+
+    const [form, setForm] = useState({
+    username: "",
+    password: ""
+    });
+
+    const handleChange = (e) => {
+    setForm({
+    ...form,
+    [e.target.name]: e.target.value
+    });
+    };
+
+    const handleSubmit = async(event) => {
+    event.preventDefault();
+    try {
+    const response = await UserService.login(form);
+    console.log("User logged in:", response);
+    localStorage.setItem("token", response.token); //token local storage for logIn persistence...do we keep it ?
+
+    } catch (error) {
+    console.error("Login failed:", error);
+    }
+
+
+    };
+
     return (
     <>
         <section className={style.content}>
@@ -11,19 +38,22 @@ export const LogInForm = () => {
             </div>
             <form className={style.form}>
                 <div className={style.userName}>
-                    {/* VALIDATION OF THE USERNAME - NEED TO BE UNIQUE - CHECK THE API */}
                     <label htmlFor="username" className={style.label}>Username</label>
-                    <input className={style.input} type="text" name='username' id="username" autoComplete='off' required/>
+                    <input className={style.input} type="text" name='username' id="username" autoComplete='off' value={form.email}
+                    onChange={handleChange} required/>
                 </div>
                 <div className={style.password}>
                     <label htmlFor="password" className={style.label}>Password</label>
-                    <input className={style.input} type="password" name='password' id="password" autoComplete='off' required/>
+                    <input className={style.input} type="password" name='password' id="password" autoComplete='off' value={form.password}
+                    onChange={handleChange} required/>
                 </div>
-            </form>
-        </section>
-        <section className={style.buttonSection}>
-            <button  className={style.button} type="submit">LOG IN</button>
-            <button  className={style.button} >CANCEL</button>
+            
+            <section className={style.buttonSection}>
+                <button  className={style.button} type="submit" onClick={handleSubmit}>LOG IN</button>
+                <button  className={style.button} >CANCEL</button>
+            </section>
+
+        </form>
         </section>
     </>
     )
