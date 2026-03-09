b100ae5d95e7b59db560f5c8c3c1b9623fa3a70c
GeraldineSaco <gsacoruiz@gmail.com>
2026-03-09 11:51:12 +0100

    fix: modified RedBackground en page Profile

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -3,21 +3,22 @@ import Title from "../../atoms/title/Title";
 import styles from './profile.module.css';
 import ArticleList from "../../organisms/ArticleList/ArticleList";
 import Footer from "../../atoms/Footer/Footer";
+import { RedBackground } from "../../atoms/RedBackground/RedBackground";
 
 export const Profile = () => {
     return (
     <>
-    <main className={styles.profileContainer}>
+    <RedBackground color={"#c8403c"} content={<SidebarMenu/>}/>
+    {/*<main className={styles.profileContainer}>
         <div className={styles.comicDivider}></div>
         <Title text="Welcome to Shop"/>
         <SidebarMenu/>
-        
         <section className="styles.content">
             <ArticleList/>
         </section>
         
     
-    </main>
+    </main>*/}
     <Footer/>
     </>
     )
