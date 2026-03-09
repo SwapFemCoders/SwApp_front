eaefadf4cc9d8d81e540a8161047dec83c282e5e
ManonChab <godfroymanon@gmail.com>
2026-03-09 12:17:43 +0100

    fix : path to Redbackground fixed

--- src/components/pages/LogIn/LogIn.jsx
+++ src/components/pages/LogIn/LogIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RedBackground } from '../../atoms/RedBackground.jsx/RedBackground'
+import { RedBackground } from '../../atoms/RedBackground/RedBackground'
 import { LogInForm } from '../../organisms/LogInForm/LogInForm'
 
 export const LogIn = () => {
