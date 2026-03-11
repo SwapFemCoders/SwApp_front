1883bd1b8831053a7d5828b408778af0fcc78de6
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-11 22:14:19 +0100

    fix, don't recovered reserve button

--- src/components/pages/Shop/Shop.jsx
+++ src/components/pages/Shop/Shop.jsx
@@ -3,11 +3,28 @@ import { AuthProvider, useAuth } from '../../../context/AuthContext';
 import Title from '../../atoms/title/Title';
 import AuthModal from '../../molecules/authModal/AuthModal';
 // import { RedBackground } from "../../atoms/RedBackground/RedBackground";
+import { FullBackground } from "../../atoms/FullBackground/FullBackground";
 import ArticleList from '../../organisms/ArticleList/ArticleList';
 import styles from './shop.module.css';
+import Footer from "../../atoms/Footer/Footer";
+import { useEffect, useState } from 'react';
+import SidebarFilters from '../../organisms/SidebarFilters/SidebarFilters';
+import ArticlesPath from '../../../services/ArticlesPath';
 
 const ShopContent = () => {
     const { isModalOpen, closeAuthModal } = useAuth();
+    const [list, setList] = useState([]);
+    const [filters, setFilters] = useState({ searchTerm: '', category: 'ALL' });
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+    //setList(MockArticles);
+        ArticlesPath().getAllArticles().then(data =>{setList(data)});
+    }, []);
+
+   const handleFilterChange = (newFilters) => {
+        setFilters(newFilters);
+    };
     return (
         <main className={styles.shopContainer}>
             <div className={styles.comicDivider}></div>
@@ -21,18 +38,19 @@ const ShopContent = () => {
 
             {/* 4. Contenido Principal (Nuestra Lista) */}
             <section className={styles.content}>
-                <ArticleList />
+                <ArticleList  filters ={filters}/>
             </section>
 
             {/* 5. Lateral Derecho */}
             <aside className={`${styles.sidebar} ${styles.right}`}>
-                <div className={styles.placeholder}>INFO / AD</div>
+                
+                <SidebarFilters 
+                    articles={[]} 
+                    filters={filters} 
+                    onFilter={handleFilterChange} 
+                />
             </aside>
-
-            {/* 6. Footer (Opcional) */}
-            <footer className={styles.footer}>
-                <div className={styles.placeholder}>FOOTER</div>
-            </footer>
+           
             <AuthModal isOpen={isModalOpen} onClose={closeAuthModal} />
         </main>
     );
@@ -40,8 +58,13 @@ const ShopContent = () => {
 
  export default function Shop() {
     return (
-        <AuthProvider >
-            <ShopContent />
-        </AuthProvider>
+         <>
+        <FullBackground content={
+            <AuthProvider >
+                <ShopContent />
+            </AuthProvider>
+        }/>
+        <Footer/>
+        </>
     );
 }
