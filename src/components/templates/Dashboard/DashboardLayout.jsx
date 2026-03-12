c7c82576c34381ceb365c5015d93bc2656f9b8dd
ManonChab <godfroymanon@gmail.com>
2026-03-12 12:47:02 +0100

    fix: connection with back progressed

--- src/components/templates/Dashboard/DashboardLayout.jsx
+++ src/components/templates/Dashboard/DashboardLayout.jsx
@@ -10,15 +10,15 @@ import SwapHistory from "../../pages/SwapHistory/SwapHistory";
 const DashboardLayout = () => {
 
   return (
-    <>
-    <div className="styles.layout">
-      <SidebarMenu  />
-    
-    <main className={styles.content}>
-      <Outlet />
-    </main>
-    </div>
-    </>
+    <section>
+      <div className="styles.layout">
+        <SidebarMenu  />
+      
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+      </div>
+    </section>
   );
 };
 
